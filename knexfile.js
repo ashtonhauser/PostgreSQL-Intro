9ed868dd2081c1b534dd9b62a4b16ce6ca17e57c
Ashton Hauser <ashton.hauser777@gmail.com>
2018-11-14 00:25:18 +0000

    Migrations implemented

---
+++ knexfile.js
@@ -0,0 +1,42 @@
+// Update with your config settings.
+const connection = require('./settings');
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
